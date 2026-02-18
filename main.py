cb6017721f8a1de84017efe53777a3c41225030e
Ahmed Zakareya <48223187+ahmedzak1@users.noreply.github.com>
2026-02-18 22:12:44 +0300

    simplifying main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
-app = FastAPI()
+from dotenv import load_dotenv
+load_dotenv(".env")
 
-@app.get("/welcome")
-def welcome():
-    return {"message": "hello world"}
+from routes import base
+
+
+app = FastAPI()
+app.include_router(base.base_router)
