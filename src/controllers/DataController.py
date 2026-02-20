e1fe795acaa0a028fac06f914ea1fc3641e78f80
Ahmed Zakareya <48223187+ahmedzak1@users.noreply.github.com>
2026-02-20 21:49:15 +0300

    return uploaded file id

--- src/controllers/DataController.py
+++ src/controllers/DataController.py
@@ -23,7 +23,7 @@ class DataController(BaseController):
 
         return True, ResponseSignal.FILE_VALIDATION_SUCCESS.value 
 
-    def generate_unique_file_name(self, original_file_name: str, project_id: str):
+    def generate_unique_filepath(self, original_file_name: str, project_id: str):
 
         random_key = self.generate_rand_strings()
         project_path = ProjectController().get_project_path(project_id=project_id)
@@ -35,7 +35,7 @@ class DataController(BaseController):
         while os.path.exists(new_file_path):
             random_key = self.generate_rand_strings()
             new_file_path = os.path.join(project_path, random_key + "_" + clean_file_name)
-        return new_file_path    
+        return new_file_path, random_key + "_" + clean_file_name  
 
     def generate_clean_file_name(self, original_file_name: str):
 
--- src/routes/data.py
+++ src/routes/data.py
@@ -27,7 +27,7 @@ async def upload_file(project_id: str, file: UploadFile, app_settings: Settings 
 
    
     project_dir_path = ProjectController().get_project_path(project_id=project_id)
-    file_path = data_controller.generate_unique_file_name(
+    file_path, file_id = data_controller.generate_unique_filepath(
      original_file_name=file.filename,
      project_id=project_id)
 
@@ -39,13 +39,15 @@ async def upload_file(project_id: str, file: UploadFile, app_settings: Settings 
     except Exception as e:
 
         logger.error(f"Error saving file: {e}")
-        
+
         return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST, content={
             "signal": ResponseSignal.FILE_UPLOAD_FAILED.value
            
         })
 
     return JSONResponse(content={
-        "signal": ResponseSignal.FILE_UPLOAD_SUCCESS.value})        
+        "signal": ResponseSignal.FILE_UPLOAD_SUCCESS.value,
+        "file_id": file_id
+        })        
 
    
