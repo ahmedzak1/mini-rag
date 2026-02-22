3af12c2be8e39fb76b7cd4324d8eb06153d1e578
Ahmed Zakareya <48223187+ahmedzak1@users.noreply.github.com>
2026-02-23 00:37:48 +0300

    refactor(schema): expose MongoDB _id as id in DataChunk model

--- src/models/db_schemas/data_chunk.py
+++ src/models/db_schemas/data_chunk.py
@@ -4,7 +4,7 @@ from typing import Optional
 
 
 class DataChunk(BaseModel):
-    _id: Optional[ObjectId] 
+    id: Optional[ObjectId] = Field(None, alias="_id")
     chunk_text: str = Field(..., min_length=1)
     chunk_metadata: dict
     chunk_order: int = Field(..., gt=0)
