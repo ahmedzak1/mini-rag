15616b563c6d1ed578ddd0507dd6bac8f5afb48f
Ahmed Zakareya <48223187+ahmedzak1@users.noreply.github.com>
2026-02-24 01:10:46 +0300

    persist uploaded file metadata in assets collection

--- src/routes/data.py
+++ src/routes/data.py
@@ -3,10 +3,11 @@ from fastapi.responses import JSONResponse
 import os
 from helpers.config import get_settings, Settings
 from controllers import DataController, ProjectController, ProcessController
-from models import ResponseSignal
+from models import ResponseSignal, AssetTypeEnum    
 from models.ProjectModel import ProjectModel
 from models.ChunkModel import ChunkModel
-from models.db_schemas import DataChunk
+from models.AssetModel import AssetModel
+from models.db_schemas import DataChunk, Asset
 from .schemas.data import ProcessRequest
 import aiofiles
 import logging
@@ -54,9 +55,22 @@ async def upload_file(request: Request, project_id: str, file: UploadFile, app_s
            
         })
 
+    #store asset record in db
+
+    asset_model = await AssetModel.create_instance(db_client=request.app.db_client)
+
+    asset_record = Asset(
+        asset_project_id = project.id,
+        asset_type = AssetTypeEnum.File.value,
+        asset_name = file_id,
+        asset_size = os.path.getsize(file_path)
+    )    
+
+    asset_record = await asset_model.create_asset(asset=asset_record)
+
     return JSONResponse(content={
         "signal": ResponseSignal.FILE_UPLOAD_SUCCESS.value,
-        "file_id": file_id
+        "file_id": str(asset_record.id)
         })        
 
    
