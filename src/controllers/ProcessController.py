9d647a4d13f7c7be43205bbfabd3d32623f17365
Ahmed Zakareya <48223187+ahmedzak1@users.noreply.github.com>
2026-02-25 01:59:34 +0300

    handle optional file loading with asset validation

--- src/controllers/ProcessController.py
+++ src/controllers/ProcessController.py
@@ -21,6 +21,9 @@ class ProcessController(BaseController):
         file_extension = self.get_file_extension(file_id=file_id)
         file_path = os.path.join(self.project_path, file_id)
 
+        if not os.path.exists(file_path):
+            return None
+
         if file_extension == ProcessingEnum.TXT.value:
             return TextLoader(file_path, encoding='utf-8')
         
@@ -32,8 +35,10 @@ class ProcessController(BaseController):
     def get_file_content(self, file_id: str):
 
         loader = self.get_file_loader(file_id=file_id)
+        if loader:
+            return loader.load()
 
-        return loader.load()    
+        return None 
 
     def process_file_content(self, file_id: str, file_content: list, chunk_size: int = 100, overlap_size: int = 20):
 
