6568ac2ab57ae2dda8351d69d2c1fa43aed6ab09
Ahmed Zakareya <48223187+ahmedzak1@users.noreply.github.com>
2026-02-27 23:52:06 +0300

    add CoHereEnum and DocumentTypeEnum

--- src/stores/llm/LLMEnums.py
+++ src/stores/llm/LLMEnums.py
@@ -7,6 +7,21 @@ class LLMEnum(Enum):
 
 class OPENAIEnum(Enum):
 
-    SYSTEM: "system"
-    USER: "user"
-    ASSISTANT: "assistant"
+    SYSTEM = "system"
+    USER = "user"
+    ASSISTANT = "assistant"
+
+
+class CoHereEnum(Enum):
+
+    SYSTEM = "SYSTEM"
+    USER = "USER"
+    ASSISTANT = "CHATBOT"
+
+    DOCUMENT = "search_document"
+    QUERY = "search_query"
+    
+class DocumentTypeEnum(Enum):
+
+    DOCUMENT = "document"
+    QUERY = "query"
