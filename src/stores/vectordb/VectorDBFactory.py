bb54d352765d1533349b77f3f1bc1162fa788111
Ahmed Zakareya <48223187+ahmedzak1@users.noreply.github.com>
2026-03-03 22:52:27 +0300

    implement VectorDBFactory to create configured vector DB providers

---
+++ src/stores/vectordb/VectorDBFactory.py
@@ -0,0 +1,18 @@
+from .VectorDBEnums import VectorDBEnums
+from .providers import QdrantDB
+from controllers.BaseController import BaseController
+
+class VectorDBFactory:
+    def __init__(self, config: dict):
+        
+        self.config = config
+        self.base_controller = BaseController()
+
+    def create(self, provider: str):
+        db_path = self.base_controller.get_vectordb_path(vectordb_name=self.config.VECTOR_DB_DISTANCE_METHOD)
+        if provider == VectorDBEnums.QDRANT.value:
+            return QdrantDB(
+                db_path=db_path,
+                distance_method=self.config.VECTOR_DB_DISTANCE_METHOD
+            )    
+        return None
