9a503f83137ddc4a4b474972bec31afed99fc70e
Ahmed Zakareya <48223187+ahmedzak1@users.noreply.github.com>
2026-03-04 23:38:22 +0300

    refactor: update startup and shutdown event handlers for better clarity and include nlp router

--- src/main.py
+++ src/main.py
@@ -1,19 +1,20 @@
 from fastapi import FastAPI
-from routes import base, data
+from routes import base, data, nlp
 from motor.motor_asyncio import AsyncIOMotorClient
 from helpers.config import get_settings
 from stores.llm.LLMProviderFactory import LLMProviderFactory
-
+from stores.vectordb.VectorDBFactory import VectorDBFactory
 
 app = FastAPI()
 
 @app.on_event("startup")
-async def startup_db_client():
+async def startup_span():
     settings = get_settings()
     app.mongo_conn = AsyncIOMotorClient(settings.MONGO_URI)
     app.db_client = app.mongo_conn[settings.MONGO_DB_DATABASE]
 
     llm_factory_provider = LLMProviderFactory(settings)
+    vectordb_factory_provider = VectorDBFactory(settings)
 
     app.generation_client = llm_factory_provider.create(provider=settings.GENERATION_BACKEND)
     app.generation_client.set_generation_model(model_id = settings.GENERATION_MODEL_ID)
@@ -21,15 +22,18 @@ async def startup_db_client():
     app.embedding_client = llm_factory_provider.create(provider=settings.EMBEDDING_BACKEND)
     app.embedding_client.set_embedding_model(model_id = settings.EMBEDDING_MODEL_ID, embedding_size = settings.EMBEDDING_MODEL_SIZE)
     
+    app.vectordb_client = vectordb_factory_provider.create(provider=settings.VECTOR_DB_BACKEND)
+    app.vectordb_client.connect()
 
 
 
 @app.on_event("shutdown")
-async def shutdown_db_client():
+async def shutdown_span():
     app.mongo_conn.close()
-
+    app.vectordb_client.disconnect()
 
 app.include_router(base.base_router)
 app.include_router(data.data_router)
+app.include_router(nlp.nlp_router)
 
 
