7645b5f3ba4e070519075e6b59f3b2df523f59c5
Ahmed Zakareya <48223187+ahmedzak1@users.noreply.github.com>
2026-03-04 23:38:42 +0300

    feat: implement NLPController with methods for managing vector DB collections

---
+++ src/controllers/NLPController.py
@@ -0,0 +1,62 @@
+from .BaseController import BaseController
+from models.db_schemas.projects import Project, DataChunk
+from typing import List
+from stores.llm.LLMEnums import DocumentTypeEnum
+
+class NLPController(BaseController):
+    def __init__(self, generation_client, embedding_client, vectordb_client):
+        super().__init__()
+
+        self.generation_client = generation_client
+        self.embedding_client = embedding_client
+        self.vectordb_client = vectordb_client
+
+    def create_collection_name(self, project_id: str):
+        return f"Collection_{project_id}".strip()
+    
+    def reset_vector_db_collection(self, project: Project):
+        collection_name = self.create_collection_name(project_id = project.project_id)
+        return self.vectordb_client.delete_collection(collection_name=collection_name)
+    
+    def get_vector_db_collection_info(self, project: Project):
+
+        collection_name = self.create_collection_name(project_id = project.project_id)
+        collection_info = self.vectordb_client.get_collection_info(collection_name=collection_name)
+
+        return collection_info
+    
+    def index_into_vectordb(self, project: Project, chunks: List[DataChunk],
+                                    do_reset: bool = False):
+        
+        collection_name = self.create_collection_name(project_id = project.project_id)
+
+        texts = [c.chunk_text for c in chunks]
+        metadata = [ c.chunk_metadata for c in chunks]
+        vector = [
+            
+            self.embedding_client.embed(text=text, document_type = DocumentTypeEnum.DOCUMENT.value)
+
+            for text in texts
+        ]
+
+        _ = self.vectordb_client.create_collection(
+            collection_name = collection_name,
+            embedding_size = self.embedding_client.embedding_size,
+            do_reset = do_reset
+
+        )
+
+        _ = self.vectordb_client.insert_many(
+            collection_name = collection_name,
+            texts = texts,
+            metadata = metadata,
+            vector = vector
+        )
+
+        return True
+
+
+    
+
+
+
