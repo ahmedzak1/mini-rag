c90d701ac63780f983775a1f095ab54579be53be
Ahmed Zakareya <48223187+ahmedzak1@users.noreply.github.com>
2026-03-07 23:35:28 +0300

    update language configs

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -38,6 +38,9 @@ class Settings(BaseSettings):
     VECTOR_DB_PATH: str
     VECTOR_DB_DISTANCE_METHOD: str = None
 
+    MAIN_LANG: str = "en"
+    DEFAULT_LANG: str = "en"
+
 
     class Config:
         env_file = ".env"
