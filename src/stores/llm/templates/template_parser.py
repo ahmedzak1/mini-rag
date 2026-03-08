bd240d78bcfa74fb05b900fd285ab6c1b05c7061
Ahmed Zakareya <48223187+ahmedzak1@users.noreply.github.com>
2026-03-08 23:09:06 +0300

    improve formatting in TemplateParser

--- src/stores/llm/templates/template_parser.py
+++ src/stores/llm/templates/template_parser.py
@@ -2,44 +2,57 @@ import os
 
 
 class TemplateParser:
-    
-    def __init__(self, language:str=None, deafault_language="en"):
+
+    def __init__(self, language: str = None, default_language: str = "en"):
         self.current_path = os.path.dirname(os.path.abspath(__file__))
-        self.default_language = deafault_language
+        self.default_language = default_language
         self.language = None
         self.set_language(language)
 
     def set_language(self, language: str):
-        if not language:
-            self.language = self.default_language
-            
-        
+
+        language = language or self.default_language
+
         language_path = os.path.join(self.current_path, "locales", language)
+
         if os.path.exists(language_path):
             self.language = language
-
         else:
             self.language = self.default_language
 
-    def get(self, group: str, key: str, vars: dict={}):
+    def get(self, group: str, key: str, vars: dict = {}):
 
         if not group or not key:
             return None
-        group_path = os.path.join(self.current_path, "locales", self.language,f"{group}.py")
+
+        group_path = os.path.join(
+            self.current_path,
+            "locales",
+            self.language,
+            f"{group}.py"
+        )
+
         target_lang = self.language
 
         if not os.path.exists(group_path):
-            group_path = os.path.join(self.current_path, "locales", self.default_language,f"{group}.py")
+            group_path = os.path.join(
+                self.current_path,
+                "locales",
+                self.default_language,
+                f"{group}.py"
+            )
             target_lang = self.default_language
+
         if not os.path.exists(group_path):
             return None
-        
-        module = __import__(f"stores.llm.templates.locales.{target_lang}.{group}", fromlist={group})
+
+        module = __import__(
+            f"stores.llm.templates.locales.{target_lang}.{group}",
+            fromlist={group}
+        )
 
         if not module:
-            return None 
-        
+            return None
+
         key_attribute = getattr(module, key)
         return key_attribute.substitute(vars)
-
-
