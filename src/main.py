0d30f761efdaf6c48e84c6e8cd0346bba3aa0e24
Ahmed Zakareya <48223187+ahmedzak1@users.noreply.github.com>
2026-03-10 23:48:59 +0300

    add async SQLAlchemy engine and session to FastAPI app startup

--- src/main.py
+++ src/main.py
@@ -1,18 +1,25 @@
 from fastapi import FastAPI
 from routes import base, data, nlp
-from motor.motor_asyncio import AsyncIOMotorClient
 from helpers.config import get_settings
 from stores.llm.LLMProviderFactory import LLMProviderFactory
 from stores.vectordb.VectorDBFactory import VectorDBFactory
 from stores.llm.templates.template_parser import TemplateParser
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker
 
 app = FastAPI()
 
 @app.on_event("startup")
 async def startup_span():
     settings = get_settings()
-    app.mongo_conn = AsyncIOMotorClient(settings.MONGO_URI)
-    app.db_client = app.mongo_conn[settings.MONGO_DB_DATABASE]
+    postgres_conn = f"postgresql+asyncpg://{settings.POSTGRES_USERNAME}:{settings.POSTGRES_PASSWORD}@{settings.POSTGRES_HOST}:{settings.POSTGRES_PORT}/{settings.POSTGRES_MAIN_DATABASE}"
+    app.db_engine = create_async_engine(postgres_conn)
+
+    app.db_client = sessionmaker(
+        app.db_engine,
+        class_ = AsyncSession,
+        expire_on_commit=False
+    )
 
     llm_factory_provider = LLMProviderFactory(settings)
     vectordb_factory_provider = VectorDBFactory(settings)
@@ -35,7 +42,7 @@ async def startup_span():
 
 @app.on_event("shutdown")
 async def shutdown_span():
-    app.mongo_conn.close()
+    app.db_engine.dispose()
     app.vectordb_client.disconnect()
 
 app.include_router(base.base_router)
