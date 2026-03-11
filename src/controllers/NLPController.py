41eedea4597827ccfd843190a81d087e3e90edb7
Ahmed Zakareya <48223187+ahmedzak1@users.noreply.github.com>
2026-03-11 23:11:58 +0300

    import new postges db sechmes

--- src/controllers/NLPController.py
+++ src/controllers/NLPController.py
@@ -1,6 +1,5 @@
 from .BaseController import BaseController
-from models.db_schemas.projects import Project
-from models.db_schemas.data_chunk import DataChunk
+from models.db_schemas import Project, DataChunk
 from typing import List
 from stores.llm.LLMEnums import DocumentTypeEnum
 import json
