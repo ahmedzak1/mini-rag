076858bdbe4941cbff6a82cfee6cca17445dd7b8
Ahmed Zakareya <48223187+ahmedzak1@users.noreply.github.com>
2026-03-12 21:56:24 +0300

    add relationships between Project and Asset

--- src/models/db_schemas/minirag/schemas/asset.py
+++ src/models/db_schemas/minirag/schemas/asset.py
@@ -14,13 +14,14 @@ class Asset(SQLAlchemyBase):
     asset_type = Column(String, nullable=False)
     asset_name = Column(String, nullable=False)
     asset_size = Column(Integer, nullable=False)
-    asset_config = Column(JSONB, nullable=False)
+    asset_config = Column(JSONB, nullable=True)
     asset_project_id = Column(Integer, ForeignKey("projects.id"), nullable=False)
 
     created_at = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
     updated_at = Column(DateTime(timezone=True), onupdate=func.now(), nullable=True)
 
     project = relationship("Project", back_populates="assets")
+    chunks = relationship("DataChunk", back_populates="asset", cascade="all, delete-orphan")
 
     __table_args__ = (
         Index('ix_asset_id', asset_project_id),
