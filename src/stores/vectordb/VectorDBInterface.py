6a6ff433516b857cc101637a1704ac4bf68cfdfc
Ahmed Zakareya <48223187+ahmedzak1@users.noreply.github.com>
2026-03-12 22:00:44 +0300

    update RetrievedDocuments import to new schema path

--- src/stores/vectordb/VectorDBInterface.py
+++ src/stores/vectordb/VectorDBInterface.py
@@ -1,6 +1,6 @@
 from abc import ABC, abstractmethod
 from typing import List
-from models.db_schemas.data_chunk import RetrievedDocuments
+from models.db_schemas import RetrievedDocuments
 
 class VectorDBInterface(ABC):
     
