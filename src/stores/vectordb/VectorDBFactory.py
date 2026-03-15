9a5486292718732bade3a7595d1b12bd3cd6f928
Ahmed Zakareya <48223187+ahmedzak1@users.noreply.github.com>
2026-03-15 23:28:46 +0300

    add pgvector provider to VectorDBFactory

--- src/stores/vectordb/VectorDBFactory.py
+++ src/stores/vectordb/VectorDBFactory.py
@@ -1,18 +1,31 @@
 from .VectorDBEnums import VectorDBEnums
-from .providers import QdrantDB
+from .providers import QdrantDB, PGVectorProvider
 from controllers.BaseController import BaseController
+from sqlalchemy.orm import sessionmaker
 
 class VectorDBFactory:
-    def __init__(self, config: dict):
+    def __init__(self, config: dict, db_client: sessionmaker):
         
         self.config = config
         self.base_controller = BaseController()
 
     def create(self, provider: str):
-        db_path = self.base_controller.get_vectordb_path(vectordb_name=self.config.VECTOR_DB_BACKEND)
+        qdrant_db_client = self.base_controller.get_vectordb_path(vectordb_name=self.config.VECTOR_DB_BACKEND)
         if provider == VectorDBEnums.QDRANT.value:
             return QdrantDB(
-                db_path=db_path,
+                db_client=qdrant_db_client,
                 distance_method=self.config.VECTOR_DB_DISTANCE_METHOD
             )    
+        
+        if provider == VectorDBEnums.PGVECTOR.value:
+            return PGVectorProvider(
+                db_client=self.db_client,
+                default_vector_size=self.config.EMBEDDING_MODEL_SIZE,
+                distance_method=self.config.VECTOR_DB_DISTANCE_METHOD,
+                index_threshold=self.config.VECTOR_DB_PGVEC_INDEX_THRESHOLD
+            )
+
         return None
+
+           
+    
